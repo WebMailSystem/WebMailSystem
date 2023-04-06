a2658972d3003d9426c94224f2f01bd3ec95f70d
Jeong gi seok <wjdrltjr5@naver.com>
2023-04-06 18:56:58 +0900

    feat : users table 구조 변경 (#5)

--- target/classes/UsersTableCreate.sql
+++ target/classes/UsersTableCreate.sql
@@ -6,7 +6,7 @@
  * Author:  정기석
  * Created: 2023. 4. 6.
  */
-
+/* 기존 user테이블을 없애고 밑에 존재하는 명령어를 실행하여 users테이블을 생성하시면 됩니다.*/
 CREATE TABLE users (
 id bigint auto_increment
 ,username VARCHAR(64) unique not null
